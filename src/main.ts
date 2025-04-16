0ee95996b9947d5cf22c56cc54ee1dd8e39e59da
Rémi <ponche62880@gmail.com>
2025-04-16 15:59:42 +0200

    🧑‍💻 Init the swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
+import { stringify } from 'querystring';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("API Swagger")
+    .setDescription("Documentation API Nest Enqueteur")
+    .setVersion('1.0')
+    .addTag("enqueteur")
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (
+      controllerKey: string,
+      methodeKey: string
+    ) => methodeKey
+
+  };
+
+  const documentationFactory = () => SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, documentationFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
